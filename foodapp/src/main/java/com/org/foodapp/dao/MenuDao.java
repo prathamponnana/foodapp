5a41e9e83676abdf907f09c683903f6a61d573b5
P D Pratham Ponnana <pdpratham.ponnana@clarivate.com>
2022-09-14 09:51:18 +0530

    pushing starter and basic files

---
+++ foodapp/src/main/java/com/org/foodapp/dao/MenuDao.java
@@ -0,0 +1,24 @@
+package com.org.foodapp.dao;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.org.foodapp.dto.Menu;
+import com.org.foodapp.repository.MenuRepository;
+
+@Repository
+public class MenuDao {
+	
+	@Autowired
+	MenuRepository menuRepository;
+	
+	public Menu saveMenu(Menu menu) {
+		return menuRepository.save(menu);
+	}
+	public Optional getMenuById(int id) {
+		return menuRepository.findById(id);
+	}
+
+}
