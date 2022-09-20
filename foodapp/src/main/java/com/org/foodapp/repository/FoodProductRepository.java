dfc9fcdc93fa244b29744b3361e5da59e259f6c9
Siddarth2 <111379198+Siddarth2@users.noreply.github.com>
2022-09-20 12:39:42 +0530

    deleting the old file

--- foodapp/src/main/java/com/org/foodapp/repository/FoodProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.org.foodapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.org.foodapp.dto.FoodProduct;
-
-public interface FoodProductRepository extends JpaRepository<FoodProduct, Integer>{
-
-}
--- foodapp/src/main/java/com/org/foodapp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.org.foodapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.org.foodapp.dto.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
